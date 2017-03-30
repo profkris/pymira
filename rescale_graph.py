f180f6552515b2a510c87baea79697f44275f71c
Simon Walker-Samuel <Simon Walker-Samuel>
2017-03-30 13:37:52 +0100

    various

--- rescale_graph.py
+++ rescale_graph.py
@@ -8,20 +8,20 @@ f = path+'LS1_spatialGraph_scaled.am'
 ps = None
 ofile = None
 
-# path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#1\\'
-# f = path+'SW1_vessel_seg.Smt.SptGraph.am'
-# ps = 4.87
-# ofile = 'SW1_spatialGraph_scaled.am'
+path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#1\\'
+f = path+'SW1_vessel_seg.Smt.SptGraph.am'
+ps = 4.87
+ofile = 'SW1_spatialGraph_scaled.am'
 
-# path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#2\\'
-# f = path+'SW2_vessel_seg.Smt.SptGraph.am'
-# ps = 6.45
-# ofile = 'SW2_spatialGraph_scaled.am'
+#path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#2\\'
+#f = path+'SW2_vessel_seg.Smt.SptGraph.am'
+#ps = 6.45
+#ofile = 'SW2_spatialGraph_scaled.am'
 
-# path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#3\\'
-# f = path+'SW3_vessel_seg_skel.SptGraph_with_radius.am'
-# ps = 11.02
-# ofile = 'SW3_spatialGraph_scaled.am'
+path = 'C:\\Users\\simon\\Dropbox\\VDA_1_lectin\\Control\\SW#3\\'
+f = path+'SW3_vessel_seg_skel.SptGraph_with_radius.am'
+ps = 11.02
+ofile = 'SW3_spatialGraph_scaled.am'
 
 from pymira import spatialgraph, statistics
 graph = spatialgraph.SpatialGraph()
