5ee1fc858902accce1f9e17c947591bc455d2a49
Simon Walker-Samuel <simon.walkersamuel@ucl.ac.uk>
2022-05-05 14:20:43 +0100

    var

--- combine_graphs.py
+++ combine_graphs.py
@@ -12,8 +12,11 @@ Required for converting Paul Sweeney's files
 import pymira.spatialgraph as sp
 import os
 import numpy as np
+join = os.path.join
 
 def merge_graphs(graph1,graph2):
+
+    # Add fields from graph2 that aren't in graph1
 
     dif1  = list(set(graph1.fieldNames) - set(graph2.fieldNames))
     dif2  = list(set(graph2.fieldNames) - set(graph1.fieldNames))
@@ -24,57 +27,71 @@ def merge_graphs(graph1,graph2):
         f['marker'] = marker
         print(('Adding {} {}...'.format(marker,fName)))
         graph1.fields.append(f)
+        
+def combine_graphs(graph1,graph2):
 
-<<<<<<< HEAD
-for i in np.arange(2,13,1):
-    #dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\SW1222\\1\\'  
-    dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\SW1222\\{}\\'.format(i)
-    #dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\LS147T - Post-VDA\\1\\'
-    #dir_ = r'C:\Users\simon\Dropbox\170606_Ben Vessel Networks\C1M3\2%'
-    #dir_ = r'C:\Users\simon\Dropbox\Glioma'
-    fn = os.path.join(dir_,'Press2Amira.txt')
-    #fp = os.path.join(dir_,'Press2Amira.txt')
-    ff = os.path.join(dir_,'Flow2Amira.txt')
-    fs = os.path.join(dir_,'Stress2Amira.txt')
-    fv = os.path.join(dir_,'Velocity2Amira.txt')
-    #mFiles = [fp,ff,fs]#,fv]
-    mFiles = [fn,ff,fv,fs]
+    # Combine fields common to both graphs
     
-    fo = os.path.join(dir_,'spatialGraph.am')
+    req_fields = ['VertexCoordinates', 'EdgePointCoordinates', 'EdgeConnectivity', 'NumEdgePoints']
+
+    # Common fields
+    fields = list(set(graph1.fieldNames).intersection(graph2.fieldNames))
     
+    add_fields = list(set(fields) - set(req_fields))
+    #breakpoint()
+    
+    for fName in req_fields:
+        f1 = graph1.get_field(fName)
+        f2 = graph2.get_field(fName)
+        if fName=='VertexCoordinates':
+            nnode1 = f1['data'].shape[0]
+            nnode2 = f2['data'].shape[0]
+        elif fName=='EdgePointCoordinates':
+            npoints1 = f1['data'].shape[0]
+            npoints2 = f2['data'].shape[0]
+        elif fName=='EdgeConnectivity':
+            nconn1 = f1['data'].shape[0]
+            nconn2 = f2['data'].shape[0]
+    
+    for fName in fields:
+        f1 = graph1.get_field(fName)
+        f2 = graph2.get_field(fName)
+        
+        if fName=='EdgeConnectivity':
+            # Offset all values by the number of nodes in graph 1
+            data = np.concatenate([f1['data'],f2['data']+nnode1])
+        else:
+            data = np.concatenate([f1['data'],f2['data']])
+        
+        graph1.set_data(data,name=fName)
+        
+    graph.set_definition_size('VERTEX',nnode1+nnode2)
+    graph.set_definition_size('EDGE',nconn1+nconn2)
+    graph.set_definition_size('POINT',npoints1+npoints2)
+
+
+if __name__=='__main__':
+    
+    path = '/mnt/data2/retinasim/cco/graph'
+    opath = path
+
     graph = sp.SpatialGraph()
-    print('Reading source graph: {}'.format(mFiles[0]))
-    graph.read(mFiles[0])
     
+    mFiles = [  'retina_artery_upper_cco.csv.am',
+                'retina_vein_upper_cco.csv.am',
+                'retina_artery_lower_cco.csv.am',
+                'retina_vein_lower_cco.csv.am',
+             ]
+    ofile = 'retina_cco.am'
+    
+    print(('Reading source graph: {}'.format(mFiles[0])))
+    graph.read(join(path,mFiles[0]))
+
     for f in mFiles[1:]:
         graph_to_add = sp.SpatialGraph()
-        graph_to_add.read(f)
-        merge_graphs(graph,graph_to_add)
-    
-    graph.write(fo)
-=======
-dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\SW1222\\1\\'  
-#dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\LS147T\\1\\'
-#dir_ = 'C:\\Users\\simon\\Dropbox\\160113_paul_simulation_results\\LS147T - Post-VDA\\1\\'
-#dir_ = r'C:\Users\simon\Dropbox\170606_Ben Vessel Networks\C1M3\2%'
-fn = os.path.join(dir_,'Net2Amira.txt')
-#fp = os.path.join(dir_,'Press2Amira.txt')
-ff = os.path.join(dir_,'Flow2Amira.txt')
-#fs = os.path.join(dir_,'Stress2Amira.txt')
-#fv = os.path.join(dir_,'Velocity2Amira.txt')
-#mFiles = [fp,ff,fs]#,fv]
-mFiles = [fn,ff]#,fv]
+        print('Merging with {}'.format(f))
+        graph_to_add.read(join(path,f))
+        combine_graphs(graph,graph_to_add)
 
-fo = os.path.join(dir_,'spatialGraph_flag.am')
-
-graph = sp.SpatialGraph()
-print(('Reading source graph: {}'.format(mFiles[0])))
-graph.read(mFiles[0])
-
-for f in mFiles[1:]:
-    graph_to_add = sp.SpatialGraph()
-    graph_to_add.read(f)
-    merge_graphs(graph,graph_to_add)
-
-graph.write(fo)
->>>>>>> a680e5459e9a481499a8c12d0867a6763350cfc5
+    graph.sanity_check()
+    graph.write(join(opath,ofile))
